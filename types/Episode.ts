caf11b1aaba85ada5b1f5aaf909f7ba3a6233a26
Audrow Nash <audrow@hey.com>
2022-01-18 09:18:52 -0800

    Create an episode type
    Signed-off-by: Audrow Nash <audrow@hey.com>

---
+++ types/Episode.ts
@@ -0,0 +1,32 @@
+type Episode = {
+  guests: string[];
+  title: string;
+  description: {
+    full: string;
+    short: string;
+  };
+  duration: {
+    hours: number;
+    minutes: number;
+    seconds: number;
+  };
+  tags: string[];
+  links: {
+    name: string;
+    url: string;
+  }[];
+  publicationDate: string;
+  filePaths: {
+    mp3: string;
+  } & Partial<{
+    coverArt: string;
+    outline: string;
+    transcript: string;
+  }>;
+  youTube: {
+    mainCode: string;
+    clipTitles?: string[];
+  }
+}
+
+export default Episode;
