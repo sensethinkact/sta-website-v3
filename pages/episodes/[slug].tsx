9753a0e0b29ac96bc6b0dbf92dac721a590bcc28
Audrow Nash <audrow@hey.com>
2022-02-10 08:53:05 -0800

    Setup episode pages from index
    Signed-off-by: Audrow Nash <audrow@hey.com>

---
+++ pages/episodes/[slug].tsx
@@ -0,0 +1,56 @@
+import { useRouter } from "next/router"
+import ErrorPage from 'next/error'
+import Head from "next/head"
+
+import getPostLoader from "../../lib/get-post-loader"
+import type { SerializedPost } from "@sta-podcast/types"
+
+type Props = {
+  post: SerializedPost
+}
+
+const Post = ({ post }: Props) => {
+  const router = useRouter()
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />
+  }
+  return (
+    <>
+      <Head>
+        <title>{post.title}</title>
+      </Head>
+      <div className="container">
+        <h1 className="title">{post.title}</h1>
+        <p>{post.description}</p>
+      </div>
+    </>
+  )
+}
+
+export default Post
+
+export async function getStaticPaths() {
+  const postLoader = await getPostLoader()
+  const slugs = postLoader.getSlugs()
+  return {
+    paths: slugs.map((slug) => ({
+      params: { slug },
+    })),
+    fallback: false,
+  }
+}
+
+type Params = {
+  params: {
+    slug: string
+  }
+}
+
+export async function getStaticProps({ params }: Params) {
+  const postLoader = await getPostLoader()
+  return {
+    props: {
+      post: postLoader.getPostBySlug(params.slug),
+    }
+  }
+}
