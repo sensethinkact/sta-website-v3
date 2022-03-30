af2cdad0eeec0a960f26c19500aa9d2f9eecd982
Audrow Nash <audrow@hey.com>
2022-03-30 08:03:51 -0700

    Add security headers
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
+const ContentSecurityPolicy = `
+  default-src 'self' discourse.ros.org;
+`
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
