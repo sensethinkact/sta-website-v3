6710f96632c01d9bccdb7fc13517426d1addfd06
Tully Foote <tfoote@osrfoundation.org>
2022-07-13 00:45:04 -0700

    Try setting referrer-policy and script-src in next RE: #57
    Signed-off-by: Tully Foote <tfoote@osrfoundation.org>

--- next.config.js
+++ next.config.js
@@ -4,3 +4,22 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+{
+  key: 'Referrer-Policy',
+  value: 'origin-when-cross-origin'
+}
+
+
+// Before defining your Security Headers
+// add Content Security Policy directives using a template string.
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' discourse.ros.org;
+`
+
+{
+  key: 'Content-Security-Policy',
+  value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+}
