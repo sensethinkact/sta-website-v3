40534f0348bc33c006409be209ef5e71e66503fe
Tully Foote <tfoote@osrfoundation.org>
2022-07-13 00:50:59 -0700

    Revert "Try setting referrer-policy and script-src in next"
    This reverts commit 6710f96632c01d9bccdb7fc13517426d1addfd06.
    
      Signed-off-by: Tully Foote <tfoote@osrfoundation.org>

--- next.config.js
+++ next.config.js
@@ -4,22 +4,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-{
-  key: 'Referrer-Policy',
-  value: 'origin-when-cross-origin'
-}
-
-
-// Before defining your Security Headers
-// add Content Security Policy directives using a template string.
-
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' discourse.ros.org;
-`
-
-{
-  key: 'Content-Security-Policy',
-  value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
-}
